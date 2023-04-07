0f3e6f71cc5b4d284441166d2fd8bdae3cad0688
yu-he <yuhe418088765@outlook.com>
2023-04-07 11:12:16 +0800

    删除无用的注释

--- src/main/java/ArtConcurrentBook/chapter07/AtomicIntegerArrayTest.java
+++ src/main/java/ArtConcurrentBook/chapter07/AtomicIntegerArrayTest.java
@@ -2,12 +2,7 @@ package ArtConcurrentBook.chapter07;
 
 import java.util.concurrent.atomic.AtomicIntegerArray;
 
-/**
- * @author tengfei.fangtf
- * @version $Id: AtomicIntegerArrayTest.java, v 0.1 2015-8-1 上午12:04:44 tengfei.fangtf Exp $
- */
 public class AtomicIntegerArrayTest {
-
     static int[] value = new int[]{1, 2};
 
     static AtomicIntegerArray ai = new AtomicIntegerArray(value);
